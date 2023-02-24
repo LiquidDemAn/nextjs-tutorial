7efa3737b721b1fc4658dbab998c8596ceb3808c
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-02-24 16:47:19 +0200

    Pages and Routes

---
+++ src/pages/contacts/contact.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+	return <h1>Dmytro</h1>;
+};
+
+export default Contact;
---
+++ src/pages/posts.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Posts = () => {
+	return (
+		<>
+			<h1>Posts list:</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos alias,
+				corporis veniam repellendus dignissimos pariatur enim minus? Dolorem
+				eum, rem exercitationem reiciendis animi repellendus, provident ullam
+				quae quidem, saepe praesentium.:
+			</p>
+		</>
+	);
+};
+
+export default Posts

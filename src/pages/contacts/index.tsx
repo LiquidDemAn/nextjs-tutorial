c1dca69cbd0e9866c782776b796a19253218df41
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-02-25 11:24:25 +0200

    Fetching data

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,13 +1,41 @@
 import Head from 'next/head';
 import { Heading } from '@/components/heading';
+import { User } from '@/typedef';
+import { GetStaticProps } from 'next';
 
-const Contacts = () => {
+type Props = {
+	contacts: User[];
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data: User[] = await response.json();
+
+	if (!data) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: { contacts: data },
+	};
+};
+
+const Contacts = ({ contacts }: Props) => {
 	return (
 		<>
 			<Head>
 				<title>Contact</title>
 			</Head>
 			<Heading text='Contacts list:' />
+			<ul>
+				{contacts.map(({ id, email, name }) => (
+					<li key={id}>
+						<strong>{name}</strong> ({email})
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
